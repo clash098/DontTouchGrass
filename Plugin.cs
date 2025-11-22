5b03a08f264c2f947f5c4b1a62f9109289b65f2f
clash <184036461+clash098@users.noreply.github.com>
2025-11-22 19:15:40 +1100

    Yes... I'm unemployed

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace DontTouchGrass
+{
+    [BepInPlugin("com.uhclash.gorillatag.DontTouchGrass", "Don't Touch Grass", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    { 
+        void Update()
+        {
+            if (!GorillaTagger.Instance) return;
+
+            CheckHandForGrass(GorillaTagger.Instance.leftHandTransform);
+            CheckHandForGrass(GorillaTagger.Instance.rightHandTransform);
+        }
+
+        void CheckHandForGrass(Transform handTransform)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(handTransform.position, -handTransform.up, out hit, 0.2f))
+            {
+                if (hit.collider.gameObject.name.ToLower() == "pit ground")
+                {
+                    System.Threading.Thread.Sleep(100);
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+        }
+    }
+}
